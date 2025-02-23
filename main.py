a06caab92b4cd563370d77cb0341f80a245e6c0b
Sokmontrey <sythatsokmontrey@gmail.com>
2025-02-22 19:04:52 -0500

    env port

--- main.py
+++ main.py
@@ -36,4 +36,7 @@ async def predict(image_path: RequestJSON):
   return response.json()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+  port = os.getenv("VISION_AGENT_API_KEY") 
+  if port is None:
+    port = 8000
+  uvicorn.run(app, host="0.0.0.0", port=port)
