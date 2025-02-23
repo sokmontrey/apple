172a45e770ac245dd475df83e0226efb0a40a2f1
Sokmontrey <sythatsokmontrey@gmail.com>
2025-02-22 22:01:43 -0500

    gemini instead

--- main.py
+++ main.py
@@ -1,40 +1,43 @@
-import requests
-from typing import Union
+from google import genai
+from google.genai import types
+from pydantic import BaseModel, TypeAdapter
 from fastapi import FastAPI
-from pydantic import BaseModel
-import uvicorn
-from fastapi.middleware.cors import CORSMiddleware
 import os
+import uvicorn
 
 app = FastAPI()
+import requests
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Allow frontend origin
-    allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods
-    allow_headers=["*"],  # Allow all headers
-)
-
-url = 'https://rn1ofmy-webendpoint.sandbox.landing.ai/inference'
+class Apple(BaseModel):
+  qualities: list[int]
 
 class RequestJSON(BaseModel):
-  image_path: str
+    image_path: str
 
-@app.post("/inference/")
-async def predict(image_path: RequestJSON):
-  print(image_path)
-  data = {
-    'image_path': image_path,
-  }
 
-  headers = {
-    "Authorization": f'Basic {os.getenv("VISION_AGENT_API_KEY")}'
-  }
+@app.post("/check-apples")
+async def call_ai_api(image_path: RequestJSON):
+    path = image_path.image_path
+    prompt = """Analyze the image to detect the number of apples present. Assess the quality of each apple based on the assigning a score from 0 to 10 (where 10 represents the highest quality and 0 represents the lowest)"""
 
-  response = requests.post(url, data=data, headers=headers)
-  return response.json()
+    image = requests.get(path)
+    
+    GENAI_API_KEY = os.getenv("GENAI_API_KEY")
+
+    client = genai.Client(api_key=GENAI_API_KEY)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-exp",
+        contents=[prompt,
+                genai.types.Part.from_bytes(data=image.content, mime_type="image/jpeg")],
+        config = {
+            'response_mime_type': 'application/json',
+            'response_schema': list[Apple],
+        })
+    apples: list[Apple] = response.parsed
+    print(apples)
+    return apples[0]
+
 
 if __name__ == "__main__":
-  port = int(os.getenv("PORT", "8080"))
-  uvicorn.run(app, host="0.0.0.0", port=port)
+    port = int(os.getenv("PORT", "8080"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
